f6315f96a1173fc67986560148ecd527963628f1
Andrea Casiraghi <a_casiraghi@outlook.com>
2025-12-16 23:24:23 -0500

    working layout

--- gui/app.py
+++ gui/app.py
@@ -4,7 +4,6 @@ import pandas as pd
 import streamlit as st
 from utils import smiles_to_svg, svg_to_datauri
 
-# init session state for prediction result and error
 if 'results_df' not in st.session_state:
     st.session_state.results_df = None
 
@@ -18,13 +17,9 @@ st.set_page_config(layout = 'wide')
 st.subheader('Property Prediction')
 st.divider(width = 'stretch')
 
-#TODO: adjust layout with containers of set height
-
 with st.container(border = False, key = 'layout_container', height = 'content'):
-    #col1, col2 = st.columns([2, 6], gap = 'small')
-                                    
-    #with col1:
-    with st.expander(label = '⚙ Settings', expanded = True, width = 'stretch'):
+
+    with st.expander(label = 'Settings', icon = ':material/settings:', expanded = True, width = 'stretch'):
         with st.container(border = False, horizontal = True, key = 'top_bar', height = 185):
             col1_1, col1_2, col1_3, col1_4 = st.columns([3, 2, 4, 1], gap = 'small')
             with col1_1:
@@ -83,9 +78,9 @@ with st.container(border = False, key = 'layout_container', height = 'content'):
             with col1_4:
                 with st.container(border = False, height = 'stretch', vertical_alignment = 'center', key = 'button_container'):
                     if not uploaded_file or not smiles_column or not id_column or not chosen_models:
-                        st.button('Predict', width = 'stretch', disabled = True, key = 'predict_button_disabled')   
+                        st.button('Predict', icon = ':material/play_circle:', width = 'stretch', disabled = True, key = 'predict_button_disabled')   
                     else:
-                        if st.button('Predict', use_container_width = True, type = 'primary', key = 'predict_button_enabled'):
+                        if st.button('Predict', icon = ':material/play_circle:', use_container_width = True, type = 'primary', key = 'predict_button_enabled'):
                                 payload_rows_df = molecules_df[[id_column, smiles_column]].rename(columns = {id_column: 'id', smiles_column: 'smiles'})
                                 payload = {'config': {'models': chosen_models}, 'data': {'rows': payload_rows_df.to_dict(orient = 'records')}}
 
@@ -99,108 +94,119 @@ with st.container(border = False, key = 'layout_container', height = 'content'):
                                     st.session_state.results_df = None
                                     st.session_state.request_error = f'Request failed with status code {r.status_code}'
 
-    #with col2:
-    with st.container(border = True, key = 'bottom_box', height = 800, vertical_alignment = 'center'):
         # TODO: add spinner when waiting for results
 
-        if st.session_state.results_df is None and st.session_state.request_error is None: 
-            #st.markdown('<div style="align-text: center; align-items: center; justify-content: center; display: flex; height: 750px;"> Load compounds and define settings to start predictions. </div>', unsafe_allow_html = True)
-            st.write('Load compounds and define settings to start predictions.')
-        elif st.session_state.request_error is not None:
-            st.error(st.session_state.request_error)
-        else:
-            with st.container(border = False, height = 800, key = 'results_container'):
-                
-                results_df = st.session_state.results_df.copy()
-                results_df['svg_text'] = results_df[smiles_column].apply(smiles_to_svg)
-                
-                # insert rendering column after ID column (second from left)
-                results_df.insert(1,
-                                  'Structure_SVG',
-                                   value = results_df['svg_text'].apply(svg_to_datauri),
-                                   allow_duplicates = True
-                                   )
-               
-                # merge predictions with molecules_df
-                results_df = results_df.merge(molecules_df.drop(columns = [smiles_column]), on = id_column)
-                # move SMILES column to end of df
-                results_df[smiles_column] = results_df.pop(smiles_column)
+    if st.session_state.results_df is None and st.session_state.request_error is None: 
+        with st.container(border = True, height = 720, vertical_alignment = 'center', horizontal_alignment = 'center', key = 'results_before_predict'):
+            
+            st.markdown(
+                "<div style='text-align: center;'>Load compounds and define settings to start predictions.</div>",
+                unsafe_allow_html = True,
+                )
+    elif st.session_state.request_error is not None:
+        st.error(st.session_state.request_error)
+    else:
+        with st.container(border = True, height = 720, key = 'results_after_predict'):
+            
+            results_df = st.session_state.results_df.copy()
+            results_df['svg_text'] = results_df[smiles_column].apply(smiles_to_svg)
+            
+            # insert rendering column after ID column (second from left)
+            results_df.insert(
+                        1,
+                        'Structure_SVG',
+                        value = results_df['svg_text'].apply(svg_to_datauri),
+                        allow_duplicates = True
+                        )
+            
+            # merge predictions with molecules_df
+            results_df = results_df.merge(molecules_df.drop(columns = [smiles_column]), on = id_column)
+            # move SMILES column to end of df
+            results_df[smiles_column] = results_df.pop(smiles_column)
 
-                results_df.drop(columns = ['svg_text'], inplace = True)
+            results_df.drop(columns = ['svg_text'], inplace = True)
+            
+            with st.container(border = False, width = 'stretch', key = 'selection_option_container'):
+
+                selection_option = st.segmented_control(
+                                                    label = 'Selection options', 
+                                                    options = ['Select all', 'Deselect all'],
+                                                    label_visibility = 'collapsed',
+                                                    default = 'Select all'
+                                                    )
                 
-                col2_1, col2_2 = st.columns([3, 1], gap = 'small')
-                with col2_2:
-                    st.space(32)
-                    with st.expander('Search by ID', width = 'stretch'):
-                        id_string = st.text_input('Search one or more IDs, separated by comma', label_visibility = 'visible')
-                        if id_string:
-                            id_list = [e.strip() for e in id_string.split(',') if e.strip()]
-                            results_df = results_df[results_df[id_column].isin(id_list)]
-                    #st.space(3)
-                    with st.expander('Numerical filters', width = 'stretch'):
+                if selection_option == 'Select all':
+                    col_selection_bool = True
+                elif selection_option == 'Deselect all':
+                    col_selection_bool = False
+            
+            col2_1, col2_2 = st.columns([3, 1], gap = 'small')
+
+            with col2_2:
+
+                with st.expander('Search by ID', width = 'stretch', expanded = False):
+                    id_string = st.text_input('Search one or more IDs, separated by comma', label_visibility = 'visible')
+                    if id_string:
+                        id_list = [e.strip() for e in id_string.split(',') if e.strip()]
+                        results_df = results_df[results_df[id_column].isin(id_list)]
+
+                with st.expander('Numerical filters', width = 'stretch', expanded = False):
+      
+                    numeric_columns = results_df.select_dtypes(include='number').columns
+
+                    with st.container(border = False, horizontal = True):
                         
-                        numeric_columns = results_df.select_dtypes(include='number').columns
+                        if st.button('\-', width = 'stretch', type = 'tertiary'):
+                            st.session_state.number_of_filters -= 1
+                        if st.button('\+', width = 'stretch', type = 'tertiary'):
+                            st.session_state.number_of_filters += 1
 
-                        with st.container(border = False, horizontal = True):
-                            if st.button('\-', width = 'stretch', type = 'tertiary'):
-                                st.session_state.number_of_filters -= 1
-                            if st.button('\+', width = 'stretch', type = 'tertiary'):
-                                st.session_state.number_of_filters += 1
+                        max_filters = 4
+                        st.session_state.number_of_filters = min(max(st.session_state.number_of_filters, 0), max_filters)
 
-                            max_filters = 4
-                            st.session_state.number_of_filters = min(max(st.session_state.number_of_filters, 0), max_filters)
-
-                        with st.container(border = False, height = 'stretch', key = 'filters_container', gap = 'small', vertical_alignment = 'distribute'):        
-                            for i in range(st.session_state.number_of_filters):
-
-                                with st.container(border = True, horizontal = True, key = f'filter_{i}'):
-                                    column_to_filter = st.selectbox('Select column to filter', 
-                                                                    options = numeric_columns,
-                                                                    label_visibility = 'collapsed', 
-                                                                    width = 'stretch',
-                                                                    key = f'col_select_{i}')
-                                    min_value = results_df[column_to_filter].min()
-                                    max_value = results_df[column_to_filter].max()
-                                    value_range = st.slider(f'{column_to_filter} slider', 
-                                                            min_value = min_value, 
-                                                            max_value = max_value, 
-                                                            value = (min_value, max_value), 
-                                                            label_visibility = 'collapsed',
-                                                            width = 'stretch',
-                                                            key = f'filter_slider_{i}')
-                                    
-                                    results_df = results_df[results_df[column_to_filter].between(*value_range)]
-
-                with col2_1:
-                    #with st.container(border = True, height = 'stretch', key = 'df_container'):
+                    with st.container(border = False, height = 'stretch', key = 'filters_container', gap = 'small', vertical_alignment = 'distribute'):        
                         
-                    selection_option = st.segmented_control(label = 'Selection options', 
-                                                            options = ['Select all', 'Deselect all'],
-                                                            label_visibility = 'collapsed',
-                                                            default = 'Select all'
-                                                            )
+                        for i in range(st.session_state.number_of_filters):
+                            
+                            with st.container(border = True, horizontal = True, height = 76, vertical_alignment = 'center', key = f'filter_{i}'):
+                                column_to_filter = st.selectbox('Select column to filter', 
+                                                                options = numeric_columns,
+                                                                label_visibility = 'collapsed', 
+                                                                width = 'stretch',
+                                                                key = f'col_select_{i}')
+                                min_value = results_df[column_to_filter].min()
+                                max_value = results_df[column_to_filter].max()
+                                value_range = st.slider(f'{column_to_filter} slider', 
+                                                        min_value = min_value, 
+                                                        max_value = max_value, 
+                                                        value = (min_value, max_value), 
+                                                        label_visibility = 'collapsed',
+                                                        width = 'stretch',
+                                                        key = f'filter_slider_{i}')
+                                
+                                results_df = results_df[results_df[column_to_filter].between(*value_range)]
+
+            with col2_1:
                     
-                    if selection_option == 'Select all':
-                        col_selection_bool = True
-                    elif selection_option == 'Deselect all':
-                        col_selection_bool = False
+                results_df.insert(
+                                0,
+                                'Selected',
+                                value = col_selection_bool,
+                                allow_duplicates = True
+                                )
 
-                    results_df.insert(0,
-                                    'Selected',
-                                    value = col_selection_bool,
-                                    allow_duplicates = True
-                                    )
-
-                    st.data_editor(results_df, 
-                                    column_config = {
+                st.data_editor(
+                            results_df, 
+                            column_config = {
                                         'Structure_SVG': st.column_config.ImageColumn(width = 'large'), 
                                         'Selected': st.column_config.CheckboxColumn(default = False, disabled = False)
                                         },
-                                    row_height = 120,
-                                    height = 640, 
-                                    hide_index = True,
-                                    disabled = range(1, len(results_df.columns))
-                                    )
-                    
-                    # TODO: download selected rows (or all rows)
+                            row_height = 120,
+                            height = 640, 
+                            hide_index = True,
+                            disabled = results_df.columns[1:]
+                            )
+                
+                # TODO: download selected rows (or all rows)
+                # TODO: show total number of rows, rows after filtering (visible rows) and number selected rows
 
